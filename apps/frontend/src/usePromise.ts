a5e43efbc658c3708eac92d1573bc87b2ff92e0f
Harry G Withers <harrygwithers@gmail.com>
2023-04-04 22:15:09 +0100

    docs: use promise docs

--- apps/frontend/src/usePromise.ts
+++ apps/frontend/src/usePromise.ts
@@ -6,6 +6,15 @@ type UsePromiseResult<T> = {
     result?: T
 }
 
+/**
+ * Converts an async operation (promise) into a reactive hook.
+ *
+ * Manages the state and error of a promise allowing for components
+ * to directly use async functions and manage their renders.
+ *
+ * @param producer Function which returns the promise
+ * @returns The state, error and result of the promise
+ */
 const usePromise = <T>(producer: () => Promise<T>): UsePromiseResult<T> => {
     const [isLoading, setIsLoading] = useState<boolean>(true)
     const [error, setError] = useState<Error>()
